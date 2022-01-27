8d10ad0dd8786512f950baa7746de9477f847990
miveko <m.kostov.igcse.ict@gmail.com>
2022-01-27 18:44:02 +0200

    Functions MoveToObject and Tween

--- GravityBounceAndMouse/main.js
+++ GravityBounceAndMouse/main.js
@@ -12,7 +12,12 @@ let randomNumber = 0
 function create() {
     Game.stage.backgroundColor = Phaser.Color.getRandomColor();
     textObject = Game.add.text(Game.width / 2, 0, text, {font: "20px Arial", fill: "#ffffff"});
-    textObject.anchor.setTo(0.5, 0)
+    textObject.anchor.setTo(0.5, 0);
+    Game.physics.enable(textObject);
+    //textObject.body.gravity.y = 300;
+    //textObject.body.bounce.y = 0.8;
+    textObject.body.collideWorldBounds = true;
+
     ball = Game.add.sprite(Game.width / 2, Game.height /2, "ball")
     ball.scale.setTo(0.1)
     ball.anchor.setTo(0.5)
@@ -25,14 +30,14 @@ function create() {
     //ball.body.bounce.setTo(1)
     ball.inputEnabled = true;
     ball.events.onInputDown.add(onMouseClick);
-    ball.input.enableDrag(false);
-    ball.body.velocity.x = 50
+    ball.input.enableDrag(true);
+    // ball.body.velocity.x = 50
 
     console.log(Game.input.mousePointer)
 
     Game.input.onTap.add(function () {
         click(Game.input.mousePointer);
-     })
+    })
 
     cursors = Game.input.keyboard.createCursorKeys()
 }
@@ -41,11 +46,23 @@ function update() {
     moveBall();
     text = "The mouse position is X=" + Game.input.mousePointer.x + " Y= " + Game.input.mousePointer.y;
     textObject.setText(text);
+   // Game.physics.arcade.moveToObject(textObject, ball, 50, 2000);
+
+   if(cursors.up.isDown) {       
+    tween = Game.add.tween(textObject).to({ x: textObject.x, y: textObject.y - 100 }, 500, Phaser.Easing.Linear.None, true, 0, 0, true)
+   }
 }
 
 function click(position) {
-//    ball.x = position.x;
-//    ball.y = position.y;
+    if(Game.input.mousePointer.x >   ball.x) {
+        ball.angle += 45;
+    } else {
+        ball.angle -= 45;
+    }
+    console.log(ball.angle);
+
+   // tween = Game.add.tween(textObject).to({ x: position.x, y: position.y }, 2000, Phaser.Easing.Bounce.InOut, true, 0, -1, true)
+   tween = Game.add.tween(textObject).to({ x: position.x, y: position.y }, 2000, Phaser.Easing.Linear.None, true, 0, 0, false)
 }
 
 function onMouseClick() {
